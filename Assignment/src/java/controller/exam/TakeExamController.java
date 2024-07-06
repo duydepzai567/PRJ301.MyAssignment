ee520d95d51343c59393ad78dce71c8d844b669f
Duy <duyaz0320@gmail.com>
2024-07-07 04:13:49 +0700

    Summary (required)
    ViewCourseController

--- Assignment/src/java/controller/exam/TakeExamController.java
+++ Assignment/src/java/controller/exam/TakeExamController.java
@@ -20,97 +20,117 @@ import controller.lio.BaseRequiredLecturerAuthenticationController;
 
 @WebServlet(name = "TakeExamController", urlPatterns = {"/exam/take"})
 public class TakeExamController extends BaseRequiredLecturerAuthenticationController {
-   
-   
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response, Account account, Lecturer lecturer)
-    throws ServletException, IOException {
-        StudentDBContext stuDB = new StudentDBContext();
-        ExamDBContext examDB = new ExamDBContext();
-        GradeDBContext graDB = new GradeDBContext();
-        
-        int cid = Integer.parseInt(request.getParameter("cid"));
-        ArrayList<Student> students = stuDB.getStudentsByCourse(cid);
-        
-        String[] raw_eids = request.getParameterValues("eid");
-        ArrayList<Integer> eids = new ArrayList<>();
-        for (String raw_eid : raw_eids) {
-            eids.add(Integer.parseInt(raw_eid));
-        }
-        
-        ArrayList<Exam> exams = examDB.getExamsByExamIds(eids);
-        ArrayList<Grades> grades = graDB.getGradesFromExamIds(eids);
-        
-        request.setAttribute("students", students);
-        request.setAttribute("exams", exams);
-        request.setAttribute("grades",grades);
-        
-        request.getRequestDispatcher("../exam/take.jsp").forward(request, response);
-        
-        
-    } 
+   @Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response, Account account, Lecturer lecturer)
+        throws ServletException, IOException {
+    String cidParam = request.getParameter("cid");
+    if (cidParam != null) {
+        try {
+            int cid = Integer.parseInt(cidParam);
+            
+            StudentDBContext stuDB = new StudentDBContext();
+            ExamDBContext examDB = new ExamDBContext();
+            GradeDBContext graDB = new GradeDBContext();
 
-    /** 
+            ArrayList<Student> students = stuDB.getStudentsByCourse(cid);
+
+            String[] raw_eids = request.getParameterValues("eid");
+            ArrayList<Integer> eids = new ArrayList<>();
+            for (String raw_eid : raw_eids) {
+                eids.add(Integer.parseInt(raw_eid));
+            }
+
+            ArrayList<Exam> exams = examDB.getExamsByExamIds(eids);
+            ArrayList<Grades> grades = graDB.getGradesFromExamIds(eids);
+
+            request.setAttribute("students", students);
+            request.setAttribute("exams", exams);
+            request.setAttribute("grades", grades);
+
+            request.getRequestDispatcher("../exam/take.jsp").forward(request, response);
+
+        } catch (NumberFormatException e) {
+            // Handle number format exception
+            e.printStackTrace();  // Log the exception for debugging
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid parameter format");
+        }
+    } else {
+        // Handle case where cid parameter is missing
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Course ID parameter is missing");
+    }
+}
+
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response,Account account, Lecturer lecturer)
-    throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response, Account account, Lecturer lecturer)
+            throws ServletException, IOException {
         int cid = Integer.parseInt(request.getParameter("cid"));
         HashSet<Integer> eids = new HashSet<>();
-        
-        String[] raw_gradeids = request.getParameterValues("gradeid");
+
         ArrayList<Grades> grades = new ArrayList<>();
+
+        String[] raw_gradeids = request.getParameterValues("gradeid");
         for (String raw_gradeid : raw_gradeids) {
-          int sid = Integer.parseInt(raw_gradeid.split("_")[0]);
-          int eid = Integer.parseInt(raw_gradeid.split("_")[1]);
-          
-          eids.add(eid);
-          
-          String raw_score = request.getParameter("score"+sid+"_"+eid);
-          if(raw_score.length()>0)
-          {
-              Grades g = new Grades();
-              Exam e = new Exam();
-              e.setId(eid);
-              
-              Student s = new Student();
-              s.setId(sid);
-              
-              g.setExam(e);
-              g.setStudent(s);
-              g.setScore(Float.parseFloat(raw_score));
-              
-              grades.add(g);
-          }
+            int sid = Integer.parseInt(raw_gradeid.split("_")[0]);
+            int eid = Integer.parseInt(raw_gradeid.split("_")[1]);
+
+            eids.add(eid);
+
+            String raw_score = request.getParameter("score" + sid + "_" + eid);
+            if (raw_score != null && !raw_score.isEmpty()) {
+                try {
+                    float score = Float.parseFloat(raw_score);
+
+                    Grades g = new Grades();
+                    Exam e = new Exam();
+                    e.setId(eid);
+
+                    Student s = new Student();
+                    s.setId(sid);
+
+                    g.setExam(e);
+                    g.setStudent(s);
+                    g.setScore(score);
+
+                    grades.add(g);
+                } catch (NumberFormatException e) {
+                    // Handle parsing error if necessary
+                    e.printStackTrace();  // Log or handle the exception
+                }
+            }
         }
-        
+
         GradeDBContext db = new GradeDBContext();
         db.insertGradesForCourse(cid, grades);
-        String url_param = "";
+
+        StringBuilder urlParams = new StringBuilder();
         for (Integer eid : eids) {
-            url_param+="&eid="+eid;
+            urlParams.append("&eid=").append(eid);
         }
-        response.sendRedirect("take?cid="+cid+url_param);
-        
-        
+        response.sendRedirect("take?cid=" + cid + urlParams.toString());
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- Assignment/web/exam/take.jsp
+++ Assignment/web/exam/take.jsp
@@ -8,43 +8,42 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
-    </head>
-    <body>
-        <form action="take" method="POST">
-            <table border="1px">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>JSP Page</title>
+</head>
+<body>
+    <form action="take" method="POST">
+        <table border="1px">
+            <tr>
+                <td></td>
+                <c:forEach items="${requestScope.exams}" var="e">
+                    <td>
+                        ${e.assessment.name}(${e.assessment.weight}) <br/>
+                        ${e.from}
+                    </td>
+                </c:forEach>
+            </tr>
+            <c:forEach items="${requestScope.students}" var="s">
                 <tr>
-                    <td></td>
+                    <td>${s.name}</td>
                     <c:forEach items="${requestScope.exams}" var="e">
                         <td>
-                            ${e.assessment.name}(${e.assessment.weight}) <br/>
-                            ${e.from}
+                            <input type="text" name="score${s.id}_${e.id}" value="
+                                <c:forEach items="${requestScope.grades}" var="g">
+                                    <c:if test="${e.id eq g.exam.id and s.id eq g.student.id}">
+                                        ${g.score}
+                                    </c:if>
+                                </c:forEach>
+                            "/>
+                            <input type="hidden" name="gradeid" value="${s.id}_${e.id}"/>
                         </td>
                     </c:forEach>
-                <tr>
-                    <c:forEach items="${requestScope.students}" var="s">
-                    <tr>
-                        <td>${s.name}</td>
-                        <c:forEach items="${requestScope.exams}" var="e">
-                            <td>
-                                <input type="text" name="score${s.id}_${e.id}"
-                                       <c:forEach items="${requestScope.grades}" var="g">
-                                           <c:if test="${e.id eq g.exam.id and s.id eq g.student.id}">
-                                               value="${g.score}"
-                                           </c:if>
-                                       </c:forEach>
-                                       />
-                                <input type="hidden" name="gradeid" value="${s.id}_${e.id}"/>
-                            </td>
-                        </c:forEach>
-                    <tr>
-
-                    </c:forEach>    
-            </table>
-            <input type="hidden" name="cid" value="${param.cid}" />
-            <input type="submit" value="save"/>
-        </form>
-    </body>
+                </tr>
+            </c:forEach>
+        </table>
+        <input type="hidden" name="cid" value="${param.cid}" />
+        <input type="submit" value="save"/>
+    </form>
+</body>
 </html>
